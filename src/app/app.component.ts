c3f783197a7aa17eb72e612f3eb2567c7f912176
Patrick Søvsø Korsgaard <patr1884@edu.eal.dk>
2017-06-08 08:29:08 +0200

    small fix + comentare

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,18 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  serverElements = [];
-  newServerName = '';
+  serverElements = [];  // server array
+  newServerName = '';   // variabler til navn og content
   newServerContent = '';
 
+
+  // Methode til at lave server
   onAddServer() {
     this.serverElements.push({
-      type: 'server',
-      name: this.newServerName,
+      type: 'server',  // her får den nye server en type
+      name: this.newServerName, // navnet er blir sat = det input fra .html filen
       content: this.newServerContent
     });
   }
-
+  // Methode til at lave serverblueprint
   onAddBlueprint() {
     this.serverElements.push({
       type: 'blueprint',
