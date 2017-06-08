aff14be06d8fa8803210771b1697019affd66ba0
Patrick Søvsø Korsgaard <patr1884@edu.eal.dk>
2017-06-08 08:46:30 +0200

    small fix + comentare 2

--- src/app/comp1/comp1.component.ts
+++ src/app/comp1/comp1.component.ts
@@ -1,13 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'; // her definere man at det skal være et component og
+                                                    // skabelonen skal hentes fra angular Core
 
 @Component({
-  selector: 'app-comp1',
-  templateUrl: './comp1.component.html',
-  styleUrls: ['./comp1.component.css']
+  selector: 'app-comp1',                    // en Selector er måden man kan hente det nye component ud på
+  templateUrl: './comp1.component.html',    // her defineres hvilken html fil der skal bruges når selectoren kaldes
+  styleUrls: ['./comp1.component.css']      // css styling. kan også fjernes og så styles direkte her
 })
 export class Comp1Component implements OnInit {
 
-  constructor() { }
+  navn: string; // Her kan alle variabler defineres og hvis man vil vhj typescript om det fx skal være string
+
+  constructor() {
+
+  }
 
   ngOnInit() {
   }
